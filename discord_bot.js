77771a6ae59759ecec0f354cee5f3172d483f8f4
jreiss1923 <joshua.reiss2@gmail.com>
2020-07-22 00:12:20 -0400

    Fixed error w/bot spawn msg

--- discord_bot.js
+++ discord_bot.js
@@ -10,18 +10,20 @@ client.on('ready', () => {
 
 client.on('guildCreate', () => {
   var guilds = client.guilds.cache.values();
-  var arr_times_joined = [];
+  var arr_guilds = [];
   for (let value of guilds) {
-    arr_times_joined.push(value);
+    arr_guilds.push(value);
   }
   var newest_guild;
-  for (let i = 0; i < arr_times_joined.length; i++) {
-    if (newest_guild === undefined || newest_guild.joinedTimestamp > arr_times_joined[i]) {
-      newest_guild = arr_times_joined[i];
+  for (let i = 0; i < arr_guilds.length; i++) {
+    console.log(arr_guilds.length);
+    if (newest_guild === undefined || newest_guild.joinedTimestamp < arr_guilds[i]) {
+      newest_guild = arr_guilds[i];
     }
   }
   var channels = newest_guild.channels.cache.values();
   for (let value of channels) {
+    console.log(value);
     if (value.deleted == false && value.type === 'text') {
       const channel = client.channels.cache.get(value.id);
       const startUpEmbed = new Discord.MessageEmbed()
@@ -30,7 +32,7 @@ client.on('guildCreate', () => {
       .setColor('#c5e300')
       .setTitle('Hi! I\'m The Plot Device!')
       .addField('My prefix is \"!\" For all my commands, type \"!help\"', "!help: Shows all commands\n !search: Search for the most recent release of a manga")
-      .setFooter("There is nothing that I don't know. I know everything.")
+      .setFooter("There is nothing that I don't know. I know everything.");
       channel.send(startUpEmbed);
       break;
     }
@@ -40,7 +42,7 @@ client.on('guildCreate', () => {
 client.on('message', msg => {
   var message_content = msg.content;
   if (message_content.length >= 9 && message_content.substring(0, 7) === '!search') {
-    var manga_title = message_content.substring(9, message_content.length);
+    var manga_title = message_content.substring(8, message_content.length);
     api.agent.login("jreiss1923", process.env.MANGADEX_PWD, false).then(() => {
       var manga = new api.Manga();
       manga.fillByQuery(manga_title).then(manga => {
@@ -64,11 +66,22 @@ client.on('message', msg => {
         .setFooter("There is nothing that I don't know. I know everything.");
         msg.channel.send(mangaEmbed);
         msg.channel.send(msg.author.toString());
+      }).catch((error) => {
+        console.log(error);
       });
-      
-    
-    });
-   }
+    }).catch((error) => {
+      const errorEmbed = new Discord.MessageEmbed()
+      .setTitle('Oops!')
+      .setAuthor('The Plot Device')
+      .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fyandere-simulator%2Fimages%2F4%2F4c%2FOugi_Oshino.png%2Frevision%2Flatest%3Fcb%3D20170408052712&f=1&nofb=1')
+      .setColor('#c5e300')
+      .addField('Mangadex has thrown an error.', error)
+      .setTimestamp()
+      .setFooter("I know nothing. It's you who knows, mangadex-kun.");
+      msg.channel.send(errorEmbed);
+      msg.channel.send(msg.author.toString());
+    })
+  }
    else if (message_content.substring(0, 5) === "!help") {
     const helpEmbed = new Discord.MessageEmbed()
     .setTitle('Help is on the way!')
@@ -80,9 +93,35 @@ client.on('message', msg => {
     msg.channel.send(helpEmbed);
     msg.channel.send(msg.author.toString());
    }
-   else if (message_content.substring(0, 5) === "!test") {
-     
-   }
+   else if (message_content.substring(0, 10) === "!searchplus" && message_content.length >= 12) {
+    var manga_title = message_content.substring(11, message_content.length);
+    api.agent.login("jreiss1923", process.env.MANGADEX_PWD, false).then(() => {
+      console.log(manga_title);
+      api.Manga.search(manga_title).then(manga => {
+        var mangaSearch = new api.Manga();
+        var allManga = [];
+        for (let i = 0; i < manga.length; i++) {
+          allManga.push(mangaSearch.fill(manga[i]));
+        }
+        return Promise.all(allManga);
+      }).then((allManga) => {
+        console.log(allManga);
+      }).catch((error) => {
+        console.log(error);
+      })
+    }).catch((error) => {
+      const errorEmbed = new Discord.MessageEmbed()
+      .setTitle('Oops!')
+      .setAuthor('The Plot Device')
+      .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fyandere-simulator%2Fimages%2F4%2F4c%2FOugi_Oshino.png%2Frevision%2Flatest%3Fcb%3D20170408052712&f=1&nofb=1')
+      .setColor('#c5e300')
+      .addField('Mangadex has thrown an error.', error)
+      .setTimestamp()
+      .setFooter("I know nothing. It's you who knows, mangadex-kun.");
+      msg.channel.send(errorEmbed);
+      msg.channel.send(msg.author.toString());
+    })
+  }
 })
 
 function convertUnixToString(unixTime) {
