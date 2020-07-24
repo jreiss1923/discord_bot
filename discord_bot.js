408f45c4138e7fe6920bcb12ff7c48691dc6090d
jreiss1923 <joshua.reiss2@gmail.com>
2020-07-24 13:57:04 -0400

    Couldn't fix mangadex api call error

--- discord_bot.js
+++ discord_bot.js
@@ -31,7 +31,7 @@ client.on('guildCreate', () => {
       .setThumbnail('https://formeinfullbloom.files.wordpress.com/2017/06/gaen5.png')
       .setColor('#c5e300')
       .setTitle('Hi! I\'m The Plot Device!')
-      .addField('My prefix is \"!\" For all my commands, type \"!help\"', "!help: Shows all commands\n !search: Search for the most recent release of a manga")
+      .addField('My prefix is \"!\" For all my commands, type \"!help\"', "To get started, !search for a manga.")
       .setFooter("There is nothing that I don't know. I know everything.");
       channel.send(startUpEmbed);
       break;
@@ -41,7 +41,7 @@ client.on('guildCreate', () => {
 
 client.on('message', msg => {
   var message_content = msg.content;
-  if (message_content.length >= 9 && message_content.substring(0, 7) === '!search') {
+  if (message_content.length >= 9 && message_content.substring(0, 8) === '!search ') {
     var manga_title = message_content.substring(8, message_content.length);
     api.agent.login("jreiss1923", process.env.MANGADEX_PWD, false).then(() => {
       var manga = new api.Manga();
@@ -67,7 +67,16 @@ client.on('message', msg => {
         msg.channel.send(mangaEmbed);
         msg.channel.send(msg.author.toString());
       }).catch((error) => {
-        console.log(error);
+        const errorEmbed = new Discord.MessageEmbed()
+        .setTitle('Oops!')
+        .setAuthor('The Plot Device')
+        .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fyandere-simulator%2Fimages%2F4%2F4c%2FOugi_Oshino.png%2Frevision%2Flatest%3Fcb%3D20170408052712&f=1&nofb=1')
+        .setColor('#c5e300')
+        .addField('Mangadex has thrown an error.', error)
+        .setTimestamp()
+        .setFooter("I know nothing. It's you who knows, mangadex-kun.");
+        msg.channel.send(errorEmbed);
+        msg.channel.send(msg.author.toString());
       });
     }).catch((error) => {
       const errorEmbed = new Discord.MessageEmbed()
@@ -82,45 +91,39 @@ client.on('message', msg => {
       msg.channel.send(msg.author.toString());
     })
   }
-   else if (message_content.substring(0, 5) === "!help") {
+  else if (message_content.substring(0, 5) === "!help") {
     const helpEmbed = new Discord.MessageEmbed()
     .setTitle('Help is on the way!')
     .setAuthor('The Plot Device')
     .setThumbnail('https://formeinfullbloom.files.wordpress.com/2017/06/gaen5.png')
     .setColor('#c5e300')
-    .addField("Here's my list of commands:", "!help: Shows all commands\n!search: Search for the most recent release of a manga")
+    .addField("Here's my list of commands:", "!help - Shows all commands\n!search <manga title> - Search for the most recent release of a manga using the mangadex search function\n!report - Report an error")
     .setFooter("There is nothing that I don't know. I know everything.");
     msg.channel.send(helpEmbed);
     msg.channel.send(msg.author.toString());
    }
-   else if (message_content.substring(0, 10) === "!searchplus" && message_content.length >= 12) {
-    var manga_title = message_content.substring(11, message_content.length);
-    api.agent.login("jreiss1923", process.env.MANGADEX_PWD, false).then(() => {
-      console.log(manga_title);
-      api.Manga.search(manga_title).then(manga => {
-        var mangaSearch = new api.Manga();
-        var allManga = [];
-        for (let i = 0; i < manga.length; i++) {
-          allManga.push(mangaSearch.fill(manga[i]));
-        }
-        return Promise.all(allManga);
-      }).then((allManga) => {
-        console.log(allManga);
-      }).catch((error) => {
-        console.log(error);
-      })
-    }).catch((error) => {
-      const errorEmbed = new Discord.MessageEmbed()
+  else if (message_content.substring(0, 7) === "!report") {
+    const reportEmbed = new Discord.MessageEmbed()
+    .setTitle('Report Help Page')
+    .setAuthor('The Plot Device')
+    .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fyandere-simulator%2Fimages%2F4%2F4c%2FOugi_Oshino.png%2Frevision%2Flatest%3Fcb%3D20170408052712&f=1&nofb=1')
+    .setColor('#c5e300')
+    .addField("Email me at joshua.reiss2@gmail.com to report any errors.", "Search errors are caused by mangadex, not by me.")
+    .setFooter("I know nothing. It's you who knows, user-kun")
+    msg.channel.send(reportEmbed);
+    msg.channel.send(msg.author.toString());
+  }
+  else if (msg.author.toString() != "<@734215154228658198>" && message_content.substring(0, 1) === "!"){
+    const errorEmbed = new Discord.MessageEmbed()
       .setTitle('Oops!')
       .setAuthor('The Plot Device')
       .setThumbnail('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fyandere-simulator%2Fimages%2F4%2F4c%2FOugi_Oshino.png%2Frevision%2Flatest%3Fcb%3D20170408052712&f=1&nofb=1')
       .setColor('#c5e300')
-      .addField('Mangadex has thrown an error.', error)
+      .addField('You used an invalid or unfinished command.', "Use !help to see the list of all valid commands.")
       .setTimestamp()
-      .setFooter("I know nothing. It's you who knows, mangadex-kun.");
+      .setFooter("I know nothing. It's you who knows, user-kun.");
       msg.channel.send(errorEmbed);
       msg.channel.send(msg.author.toString());
-    })
   }
 })
 
@@ -128,6 +131,11 @@ function convertUnixToString(unixTime) {
   var date = new Date(unixTime * 1000);
   return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear();
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 client.login(process.env.BOT_KEY);
 
